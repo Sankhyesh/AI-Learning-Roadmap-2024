f57099eeab33f3f41c1c8e2ce1e26e17aa93b939
Sankhyesh Singh Thakur <sankhyesh3@gmail.com>
2025-05-06 02:15:38 +0530

    Refactor code structure for improved readability and maintainability

--- coding/stack/builiding_concepts.cpp
+++ coding/stack/builiding_concepts.cpp
@@ -75,78 +75,40 @@
 
 // NGR
 
-// #include <iostream>
-// #include <vector>
-// #include <stack>
-// using namespace std;
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
 
-// vector<int> ngr(vector<int> arr, int n){
-//     vector<int> ans(n, -1);
-//     stack<int> st;
-//     for(int i = n -1; i >=0; i--){
-//         while(!st.empty() && st.top() <= arr[i] ){
-//             st.pop();
-//         }
-//         if(!st.empty()){
-//             ans[i] = st.top();
-//         }
-//         st.push(arr[i]);
-//     }
-//     return ans;
-// }
+vector<int> ngr(vector<int> arr, int n)
+{
+    vector<int> ans(n, -1);
+    stack<int> st;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty() && st.top() <= arr[i])
+        {
+            st.pop();
+        }
+        if (!st.empty())
+        {
+            ans[i] = st.top();
+        }
+        st.push(arr[i]);
+    }
+    return ans;
+}
 
-// int main(){
-//     vector<int> arr = {2, 3, 4, 7, 2, 5, 2};
+int main()
+{
+    vector<int> arr = {2, 3, 4, 7, 2, 5, 2};
+    vector<int> ans = ngr(arr, arr.size());
+    // Print using reverse iterators on a single line
+    for (auto it = ans.rbegin(); it != ans.rend(); ++it)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
 
-//     vector<int> ans = ngr(arr, arr.size());
-
-//     // for(int i = ans.size() -1; i>=0; i--){
-//     //     cout << ans[i] << endl;
-//     // }
-
-//     for (auto i: ans){
-//         cout<<i<<endl;
-//     }
-//     return 0;
-// }
-
-// NSL
-// #include <iostream>
-// #include <vector>
-// #include <stack>
-// using namespace std;
-
-// vector<int> nsl(vector<int> arr, int n)
-// {
-//     stack<int> st;
-//     vector<int> ans(n, -1);
-//     for (int i = 0; i < n; i++)
-//     {
-//         while (!st.empty() && arr[i] <= st.top())
-//         {
-//             st.pop();
-//         }
-//         if (!st.empty())
-//         {
-//             ans[i] = st.top();
-//         }
-//         st.push(arr[i]);
-//     }
-//     return ans;
-// }
-// int main()
-// {
-//     vector<int> arr = {1, 3, 4, 5, 1};
-//     // 3 1 4
-//     // -1 1
-//     vector<int> ans = nsl(arr, arr.size());
-//     for (auto i : ans)
-//     {
-//         cout << i << endl;
-//     }
-//     return 0;
-// }
-
-
-
-
+    return 0;
+}
